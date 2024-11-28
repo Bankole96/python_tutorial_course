dcd119bb7b94f0f5d5687cc45c3fe95d3baa73eb
bankole96 <bankoledare96@gmail.com>
2024-11-28 15:33:03 +0000

    this is first python repo

---
+++ playground/variables_and_values/variable.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#we are declaring a variable and intializing it a value of integer
+
+X = 10  #the equal sign(=) in maths, is know as the assignment operator in python language.
+        #the value you assign or give to a variable determines the type of that variable, X = 10 is type integer
+print(X)
+print(type(X))
+
+name ='Rianne'
+print(name)
+print(type(name))
+
+X = 'yankee'
+print(X)
+print(type(X))
+
+Essay = '''I LOVE PROGRAMMING.
+I AM AMBITIOUS TO LEARN WITH ALL COST'''
+
+print(Essay)
+print(type(Essay))
+
+A = B = C = 30
+print("A = ", A)
+print("B = ", B)
+print("C = ", C)
+
+name, age, occupation = "Damilare", 46, "Python Programmer"
+print(name, age, occupation)
+print("name = ", name)
+print("age = ", age)
+print("occupation = ", occupation)
+print(name + occupation)
+
+
